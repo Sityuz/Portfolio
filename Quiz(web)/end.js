1832de0a050ad63aa605442aac88b4f07d5a2902
Sityuz <105504723+Sityuz@users.noreply.github.com>
2025-07-30 12:15:09 +0500

    end.js

--- Quiz(web)/end.js
+++ Quiz(web)/end.js
@@ -29,7 +29,7 @@ saveHighScore = e =>{
     highScores.splice(5)
 
     localStorage.setItem("highScores", JSON.stringify(highScores))
-    window.location.assign("/")
+    window.location.assign("./")
 
     console.log(highScores);
 }
