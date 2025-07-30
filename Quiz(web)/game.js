df329c757554055178704d15d7061f9aea2c9668
Sityuz <105504723+Sityuz@users.noreply.github.com>
2025-07-30 12:32:56 +0500

    game.js

--- Quiz(web)/game.js
+++ Quiz(web)/game.js
@@ -60,7 +60,7 @@ getNewQuestion = () => {
 
         localStorage.setItem('mostRecentScore',score)
 
-        return window.location.assign("/end.html")}
+        return window.location.assign("./end.html")}
     questionCounter++;
 
     progressText.innerText = "Question "+ questionCounter+ "/" + MAX_QUESTION;
